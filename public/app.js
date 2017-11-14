d591b0f71f26a84e95a0bf3352ee59e608129294
itkazmi814 <itkazmi814@gmail.com>
2017-11-14 08:05:09 -0600

    DOM manip effects for hide/show comments done

--- public/app.js
+++ public/app.js
@@ -11,8 +11,32 @@ $(".save-article-btn").on("click", function(event) {
 
 $(".view-comments-btn").on("click", function(event) {
 	const articleId = $(this).attr("data-article-id");
-	console.log(this);
-	$(this).show();
-	$(`#${articleId}`).removeClass("hidden-xs-up");
-	console.log(this);
+	console.log(articleId);
+	console.log($(`#comments-${articleId}`));
+	console.log($(`#comments-${articleId}`).attr("data-visible"));
+	//If the comments are not displayed, make them visible
+	if ($(`#comments-${articleId}`).attr("data-visible") === "no") {
+		$(`#create-comment-${articleId}`).show();
+		$(`#comments-${articleId}`).slideDown("fast");
+		$(`#comments-${articleId}`).attr("data-visible", "yes");
+		//If comment are displayed, hide them and the create comment form
+	} else {
+		$(`#form-${articleId}`).slideUp("fast");
+		$(`#comments-${articleId}`).slideUp("fast");
+		$(`#comments-${articleId}`).attr("data-visible", "no");
+		$(`#form-${articleId}`).attr("data-visible", "no");
+	}
+});
+
+$(".create-comment-btn").on("click", function(event) {
+	const articleId = $(this).attr("data-article-id");
+	console.log(articleId);
+	console.log($(`#form-${articleId}`));
+	console.log($(`#form-${articleId}`).attr("data-visible"));
+	//Hide the Add Comment button and display the new comment form
+	if ($(`#form-${articleId}`).attr("data-visible") === "no") {
+		$(`#create-comment-${articleId}`).slideUp("fast");
+		$(`#form-${articleId}`).slideDown("fast");
+		$(`#form-${articleId}`).attr("data-visible", "yes");
+	}
 });
